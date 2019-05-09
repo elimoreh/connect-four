68c24a9188d4e4285ece9ee03c937dcf891214f5
elimoreh <elimoreh@berkeley.edu>
2019-05-08 17:25:26 -0700

    databases schema setup

--- game/database/schema.sql
+++ game/database/schema.sql
@@ -1,1 +1,15 @@
-DROP DATABASE scoreboard_table IF EXISTS;
+DROP DATABASE IF EXISTS scoreboard_data;
+CREATE DATABASE scoreboard_data;
+USE scoreboard_data;
+
+DROP DATABASE IF EXISTS scoreboard;
+CREATE TABLE scoreboard
+(
+  id INT AUTO_INCREMENT UNIQUE,
+  player VARCHAR(20) NOT NULL,
+  score INT
+);
+
+INSERT INTO scoreboard(player, score) VALUES ("red", 0), ("yellow", 0);
+
+
